05cc6f27556d38e6fe204f06b1d18c13b8f70e28
Vinzenz Muser <vinzenz.muser@uzh.ch>
2017-01-03 09:50:34 +0100

    better plot for debugging

--- plot_all.m
+++ plot_all.m
@@ -11,12 +11,7 @@ pause(0.01);
 subplot(2,2,2)
 hold on;
 cam = plotCamera('Location',T(1:3, 4)','Orientation',T(1:3, 1:3),'Opacity',0);
-
-subplot(2,2,3)
-hold on;
-pos = T(1:3,4);
-scatter3(pos(1),pos(2),pos(3),20,'o')
-scatter3(S1.p3D(1,:),S1.p3D(2,:),S1.p3D(3,:),2,'x')
+plot3(S1.p3D(1,:),S1.p3D(2,:),S1.p3D(3,:),'x')
 
 end
 
