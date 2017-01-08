1d816817d89b859769af15600e3cb47c3ae91358
Vinzenz Muser <vinzenz.muser@uzh.ch>
2017-01-08 21:36:27 +0100

    cleaned up code

--- plot_all.m
+++ plot_all.m
@@ -1,12 +1,13 @@
 function [] = plot_all( image,S1,T, route )
-%PLOT_ALL Summary of this function goes here
-%   Detailed explanation goes here
-%close all;
+%Creates the differnt plots of the current state. Namely a plot of the
+%movie with the keypoints, the trajectory, a point cloud and a point cloud
+%with the trajectory
 figure(1)
-%figure('units','normalized','outerposition',[0 0 1 1]);
+
 % get the figure and axes handles
 hFig = gcf;
 hAx  = gca;
+
 % set the figure to full screen
 set(hFig,'units','normalized','outerposition',[0 0 1 1]);
  
@@ -20,6 +21,7 @@ end
 
 % Makes sure that plots refresh.    
 pause(0.01);
+
 subplot(2,2,2)
 hold on;
 %location = -T(1:3, 1:3)'*T(1:3, 4);
@@ -31,20 +33,14 @@ view([0 0]);
 subplot(2,2,3)
 hold on;
 pos = T(1:3,4);
-%plot3(pos(1),pos(2),pos(3),20,'x')
-%cam = plotCamera('Location',T(1:3, 4)','Orientation',T(1:3, 1:3),'Opacity',0, 'Size', 4);
-%plotCoordinateFrame(T(1:3, 1:3),T(1:3, 4), 100);
-%plot3(S1.p3D(1,:),S1.p3D(2,:),S1.p3D(3,:),2,'o')
 scatter3(S1.p3D(3,S1.corr),S1.p3D(2,S1.corr),S1.p3D(1,S1.corr),2,'o')
 b = 20;
 bz = [-5, 130];
 axis([bz, -b, b, -b, b]);
-%axis equal
 rotate3d on;
 grid
 
 subplot(2,2,4);
-% plot3(route(3,:), route(2,:), route(1,:));
 scatter3(S1.p3D(1,S1.corr),S1.p3D(2,S1.corr),S1.p3D(3,S1.corr))
 hold on
 axis equal;
